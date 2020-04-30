c664ac222cbcc428b48273277ab657e2365e6441
harrymkwn <heratmakwana19@gmail.com>
2020-04-30 17:22:58 +0530

    router completed added to client side

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
-import React from 'React'   
+import React from 'react'   
 import ReactDOM from 'react-dom'
 
 import App from './App'
 
 
-ReactDOM.render(<App/>,document.querySelector('#root'));
+ReactDOM.render(<App/>,document.getElementById('root'));
